c0be1fb313d3e52950d831a9f00de9d90f1ac2a8
iliasaw <62304861+iliasaw@users.noreply.github.com>
2022-02-05 16:44:27 +0300

    Add files via upload

---
+++ botconfig.js
@@ -0,0 +1,42 @@
+module.exports = {
+  Admins: ["833957508979752960"], //Admins of the bot
+  ExpressServer: true, //If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://discord.gg/bxkhsCM8Ux", //Support Server Link
+  Token: process.env.Token || "ODQ3MTExOTAzOTIyODgwNTEz.YK5UcA.RQAtUyrx5_M9BTsK79f3tOAH9vM", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "837317601356415007", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "g6_J2ngtREg3YJhKbNL4-ROKpG3mmyy0", //Discord Client Secret
+  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+  ServerDeafen: true, //If you want bot to stay deafened
+  DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
+  CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
+  "24/7": false, //If you want the bot to be stay in the vc 24/7
+  CookieSecret: "GBF Top", //A Secret like a password
+  IconURL:
+    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
+  Permissions: 2205281600, //Bot Inviting Permissions
+  Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
+  
+  Presence: {
+    status: "online", // You can show online, idle, and dnd
+    name: "Музыку", // The message shown
+    type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+  },
+
+  //Lavalink
+  Lavalink: {
+    id: "Main",
+    host: "node05.lavalink.eu",
+    port: 2333, // The port that lavalink is listening to. This must be a number!
+    pass: "Raccoon",
+    secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
+  },
+
+  //Please go to https://developer.spotify.com/dashboard/
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || "2edfb782e9214574a8362bc271a45a70", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "2edfb782e9214574a8362bc271a45a70", //Spotify Client Secret
+  },
+};
